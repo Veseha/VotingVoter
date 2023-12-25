f015acb7039f70480515aa248da0b3f4a8bf960b
OverclockingInfernale <mirasr56@gmail.com>
2023-12-26 00:31:20 +0600

    Entities updated

--- src/main/java/votingvoter/Model/Answer.java
+++ src/main/java/votingvoter/Model/Answer.java
@@ -1,4 +1,22 @@
 package votingvoter.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "Answer")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String answerName;
+
+    private String answerText;
 }
